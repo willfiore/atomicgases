a2fbada6e79bc29ead600f8f74e6f16a40b18ed0
Will Fiore <w.p.fiore@gmail.com>
2017-02-16 12:41:23 +0000

    Cleaned up, added comments, docstrings.

--- code/single_two_level_atom/direct_method.py
+++ code/single_two_level_atom/direct_method.py
@@ -1,30 +1,66 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  8 13:59:51 2017
+### TODO:
+### this method is currently not working correctly.
 
-@author: wfior
-"""
+########################
+### direct_method.py ###
+########################
+
+### Description:
+# simulating transition probability for a single two-level atom
 
 import math
 import random
 import matplotlib.pyplot as plot
 import numpy as np
 
-k = 1
+k = 1 # transition rate
 
-def time_from_probability (prob) :
+def time_from_probability (prob):
+    """ Returns the time at which a generated probability would place the
+        atom in an excited state.
+        
+    Args:
+        prob (float): probability of which to find the time for.
+            
+    Returns:
+        time (float)
+            
+    """
+    
     return ((-1/k) * math.log(1 - prob))
     
 def generate_one_atom(n, p):
+    """ Generate times and directions of state flips for a single atom
+    
+    Args:
+        n (int): number of state flips to generate
+        p (bool): Whether to plot the final result
+        
+    Returns:
+        (times[], flips[]), where times[] is an array of flip times and
+        flips[] is the direction of the corresponding flips.
+    
+    """
+    
+    # Create initial arrays to store times and flips
     times = [0]
     flips = [0]
+
     for i in range(n):
+        
+        # Generate time from random probability roll.
         time = time_from_probability(random.random()/2)
+        
         times.append(time)
         times.append(time)
         
+        # Flip always opposes the direction of the previous flip
         flips.append(flips[-1])
         flips.append(1 if flips[-1] == 0 else 0)
+        
+        # Note how the times and flips are doubled up so the resulting plot
+        # consists of vertical, direct state changes instaed of diagonal lines
+        # (interpolating flips makes no sense)
     
     times.sort()
     
@@ -35,6 +71,13 @@ def generate_one_atom(n, p):
     return times, flips
 
 def generate_curve(n, bins):
+    """ Generate average state for many atoms
+    
+    Args:
+        n (int): number of atoms to simulate
+        bins (int): Number of bins to use for averaging flips from multiple atoms
+    """
+    
     times = []
     for i in range(n):
         times.extend(generate_one_atom(100, False)[0])
@@ -45,18 +88,18 @@ def generate_curve(n, bins):
     final_times = np.linspace(min_time, max_time, bins)
     final_counts = []
 
+    # sum the total number of flips in this bin
     for i in range(bins-1):
         bin_min = final_times[i]
         bin_max = final_times[i+1]
         count = sum(bin_min < t < bin_max for t in times)
         final_counts.append(count)
-        
-    final_counts.append(0)
-    
-    final_counts = [x / n for x in final_counts]
-    print(len(final_times))
-    print(len(final_counts))
+
+    # make sure final_counts is the same length as final_times        
+    final_counts.append(0)    
+    final_counts = [x / n for x in final_counts] # normalize
     
     plot.plot(final_times, final_counts)
         
-generate_curve(20, 100)
+# generate curve
+generate_curve(200, 1000)
--- code/single_two_level_atom/timestep_method.py
+++ code/single_two_level_atom/timestep_method.py
@@ -1,60 +1,99 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+##########################
+### timestep_method.py ###
+##########################
 
-This is a temporary script file.
-"""
+### Description:
+# simulating transition probability for a single two-level atom
 
 import math
 import random
 import matplotlib.pyplot as plt
 
-k = 1
+k = 1 # transition rate
 
-def get_probability(time):
+def get_excited_probability(time):
+    """ Gets the probability of a single atom being in an excited state at
+        a specific time.
+    
+    Args:
+        time (float): time at which to find the probability
+        
+    """
     return 0.5*(1-math.exp(-2*time*k))
 
-def generate_state(plot=False):
+def generate_state(max_t=3, dt=0.01, plot=False):    
+    """ Generates the state over time for a single atom
     
+    Args:
+        max_t (float): duration of time to simulate (in seconds)
+        dt    (float): timestep interval (in seconds)
+        plot   (bool): whether to plot the result afterwards
+        
+    Returns:
+        (time[], state[]), where time[] is an array of all generated time values
+        from 0 to max_t, and state[] is the corresponding states (either 0 or 1)
+        for each time.
+        
+    """
+    
+    # starting time
     t = 0
-    max_t = 3
-    dt = 0.01
-
-    time = [0]    
+    
+    time = [t]
     state = [0]
     
     while t < max_t:
         t += dt
         
+        # when plotting, we want to double up points so straight vertical lines
+        # are drawn between transitions as opposed to diagonals (interpolating
+        # between the two states 0 and 1 doesn't make sense)
         if (plot):
             time.append(t)
             state.append(state[-1])
             
         time.append(t)
-        state.append(1 if random.random() < get_probability(t) else 0)
+        # generate a random probability between 0 and 1 and see if its below
+        # the threshold for being in the excited state. then put the corresponding
+        # state into the state array.
+        state.append(1 if random.random() < get_excited_probability(t) else 0)
         
     if (plot):
         plt.figure(figsize=(15, 5), dpi=80)
         plt.plot(time, state)
+        
     return time, state
 
-def average_state():
-    N = 10000
+def average_state(N=10000):
+    """ Averages the state over many atoms and plots the result
+    
+    Args:
+        N (int): number of atoms to simulate
+    
+    """
+    
+    # Grab initial time and state
     time = generate_state()[0]
     state = generate_state()[1]
-    
+
     for i in range(N - 1):
         new_state = generate_state()[1]
+        # Perform elementwise addition on the existing states with new states
         state = [sum(x) for x in zip(state, new_state)]
         
+    # Normalise the final state
     state = [x / N for x in state]
-            
+
     plt.figure(2)
     plt.plot(time, state)
     
-    average_state = [get_probability(t) for t in time]
+    # Compare the plot with the exact probability function
+    average_state = [get_excited_probability(t) for t in time]
     plt.plot(time, average_state, 'r')
-    
-generate_state(True)
+
+# Generate single example state, and plot it    
+generate_state(3, 0.01, True)
 plt.ylim(-0.5, 1.5)
-average_state()
+
+# Generate average state, and plot it
+average_state(1000)
