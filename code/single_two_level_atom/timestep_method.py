6380a262f7beba16e0797bf4dd32d4bc84d91790
Will Fiore <w.p.fiore@gmail.com>
2017-02-16 17:34:30 +0000

    Added generated figures & new notes

--- code/single_two_level_atom/timestep_method.py
+++ code/single_two_level_atom/timestep_method.py
@@ -59,7 +59,7 @@ def generate_state(max_t=3, dt=0.01, plot=False):
         state.append(1 if random.random() < get_excited_probability(t) else 0)
         
     if (plot):
-        plt.figure(figsize=(15, 5), dpi=80)
+        plt.figure(figsize=(8, 3), dpi=80)
         plt.plot(time, state)
         
     return time, state
@@ -91,9 +91,17 @@ def average_state(N=10000):
     average_state = [get_excited_probability(t) for t in time]
     plt.plot(time, average_state, 'r')
 
-# Generate single example state, and plot it    
-generate_state(3, 0.01, True)
+# Generate single example state, and plot it
+max_t = 2
+generate_state(max_t, 0.01, True)
 plt.ylim(-0.5, 1.5)
+plt.xlim(0, max_t)
+plt.tick_params(
+    axis='y',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    left='off',      # ticks along the bottom edge are off
+    right='off',         # ticks along the top edge are off
+    labelleft='off') # labels along the bottom edge are off
 
 # Generate average state, and plot it
-average_state(1000)
+average_state(10000)
